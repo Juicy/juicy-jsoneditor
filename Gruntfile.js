ae2056a988133342b1c8a5519c747f7152fb0cbf
Marcin Warpechowski <warpech@gmail.com>
2015-06-17 13:43:32 +0200

    json files no longer contain version

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,10 +25,10 @@ module.exports = function(grunt) {
         },
         bump: {
           options: {
-            files: ['package.json', 'bower.json', 'src/*'],
+            files: ['src/*'],
             commit: true,
             commitMessage: '%VERSION%',
-            commitFiles: ['package.json', 'bower.json', 'src/*'],
+            commitFiles: ['src/*'],
             createTag: true,
             tagName: '%VERSION%',
             tagMessage: 'Version %VERSION%',
